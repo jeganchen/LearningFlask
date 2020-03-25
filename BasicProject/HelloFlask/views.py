068f2ced259ccade6b0eb1689d8606248224da91
Jegan <jzgchen@163.com>
2020-03-25 13:26:38 +0800

    first project done

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 from HelloFlask import app
+from datetime import datetime
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello Flask From Views!"
+    now = datetime.now()
+    format_date = now.strftime("%A, %d %B, %Y at %X")
+    title = "Hello Flask"
+    message = "Hello Flask!"
+    content = "on " + format_date
+    return render_template("index.html", title=title, message=message, content=content)
+
+@app.route('/about')
+def about():
+    return render_template("about.html",
+                           title='About Flask',
+                           content='all about flask')
