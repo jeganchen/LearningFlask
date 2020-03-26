279006c39dcabc4b20bb7be57129ac05d8d183a0
Jegan <jzgchen@163.com>
2020-03-26 14:08:42 +0800

    upload suc

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -3,6 +3,8 @@ from HelloFlask import app
 from datetime import datetime
 from werkzeug.utils import secure_filename
 import openpyxl
+import os
+import pyodbc
 
 
 @app.route('/')
@@ -31,16 +33,53 @@ def upload():
             flash('No file part')
             return redirect(request.url)
         file = request.files['fileUpload']
-        file.save("C:\\Users\\chenzige\\source\\repos\\LearningFlask\\BasicProject\\HelloFlask\\tempfiles\\"+secure_filename(file.name)+'.xlsx')
-        wb = openpyxl.load_workbook("C:\\Users\\chenzige\\source\\repos\\LearningFlask\\BasicProject\\HelloFlask\\tempfiles\\"+file.name+'.xlsx')
-        ws = wb.active
-        rows = ws.max_row
-        cols = ws.max_column
-        data=ws.cell(row=rows,column=cols).value 
-        result = (cell.value for cell in row for row in ws[1:4] )
-        #values = ' '
-        #for cell in row:
-        #    values+=cell.value
-        return data
+        UPLOAD_FOLDER = './HelloFlask/tempfiles/{}.xlsx'.format(str(int(datetime.now().timestamp()*100)))   # 第一种方法
+        ROOT_FOLDER = os.path.join(os.getcwd(), UPLOAD_FOLDER)  # 整合绝对路径
+        file.save(ROOT_FOLDER)
+      #  retrun render_template("upload.html", result=insert_table(ROOT_FOLDER))
+     #   return load_excel(ROOT_FOLDER) #
+        return render_template("upload.html", result=insert_table(ROOT_FOLDER))
+
+def load_excel(ROOT_FOLDER):
+    wb = openpyxl.load_workbook(ROOT_FOLDER)
+    ws = wb.active
+    rown = ws.max_row
+    coln = ws.max_column
+    content = '<table name=data border="1"> '
+    for row in ws.iter_rows(max_row=rown, max_col=coln):
+        content += ' <tr> '
+        for cell in row:
+            content += ' <td> '+ str(cell.value) + '</td>'
+        content += '</tr>'
+    content +='</table>'
+    return content
+
+def insert_table(ROOT_FOLDER):
+    connection = pyodbc.connect(
+        driver='{iSeries Access ODBC Driver}',
+        system='gitdev',
+        uid='chanfree',
+        pwd='mattel01')
+    c1=connection.cursor()
+
+    wb = openpyxl.load_workbook(ROOT_FOLDER)
+    ws = wb.active
+    rown = ws.max_row
+    coln = ws.max_column
+    content = ' '
+    for row in ws.iter_rows(min_row=2, max_row=rown, max_col=coln):
+        for cell in row:
+            if content != ' ':
+                content +=","
+            content += "'"+ str(cell.value) + "'"
+        sql = 'insert into devjegan.TRUMSPTM values ({})'.format(content)
+        content = ' '
+        print(sql)
+        c1.execute(sql)
+    c1.execute('select COMPTR , TOYNTR , ORNOTR,FOBPTR,SELYTR,EFDTTR,ETDTTR,SDOCTR,URGTTR from devjegan.TRUMSPTM ')
+    connection.commit()
+    return c1
+    
+
 
 
